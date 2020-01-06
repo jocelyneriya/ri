c1dad3e5de3c8132d5b01c1172dc9c180f70390d
gowsi <52281274+jocelyneriya@users.noreply.github.com>
2020-01-06 12:32:19 +0530

    Create 400array.py

---
+++ 400array.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+t=list(set(l))
+r=[]
+for i in range(len(t)):
+    r.append(l.count(t[i]))
+c=0
+for i in range(len(l)-1):
+    k=0 
+    for j in range(len(t)):
+        if(l[i]==t[j]):
+            k=r[j]
